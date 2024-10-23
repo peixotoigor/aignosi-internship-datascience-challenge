d9891e0fb6aa7c7bd65083c93dda76ba964ea125
Igor Peixoto <igorpeixoto_@outlook.com.br>
2024-10-23 12:54:20 -0300

    Tratamento inical dos dados e geração de graficos das series temporais

---
+++ eda_mining_ore.py
@@ -0,0 +1,251 @@
+# Importação de bibliotecas
+
+# Manipulação e tratamento de dados
+import pandas as pd
+import numpy as np
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+# Visualização de dados
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Carregando o dataset
+file_path= "base/MiningProcess_Flotation_Plant_Database.csv"
+
+dados = pd.read_csv(
+    file_path,
+    decimal=',',
+)
+
+# Pré-processamento
+
+print(dados.head()) # Exibir as primeiras linhas do dataset
+print(dados.info()) # Obteção de informações do dataset
+print(dados.describe()) # Verificar a distribuição dos valores de cada coluna
+
+# A coluna data está formatada como objet. É necessário transformá-la em um tipo data
+# Não existe números nulos e nem números negativos, indicando que não há valores fora do esperado
+
+dados['date'] = pd.to_datetime(dados['date'], format='%Y-%m-%d %H:%M:%S') # Conversão da coluna 'date' para datetime
+dados.set_index('date', inplace=True) # Definir a coluna 'date' como índice do DataFrame original
+print(dados.info()) # Verificar a mudança de tipo da coluna 'date'
+
+# Verificar linhas duplicadas
+linhas_duplicadas = dados.duplicated().sum()
+#print(dados[dados.duplicated()]) # Exibir as linhas duplicadas
+dados_iniciais = dados.shape[0] # Número de linhas do dataset inicial
+porcentagem_duplicadas = (linhas_duplicadas / dados_iniciais) * 100
+print("Existem {} linhas duplicadas, o que equivale a {:.2f}% do dataset inicial.".format(linhas_duplicadas, porcentagem_duplicadas))
+
+# Verificar se existem dias faltosos no intervalo de tempo
+dados_faltosos = dados[~dados.index.duplicated(keep='first')] # Remover duplicatas do índice
+date_range = pd.date_range(start=dados_faltosos.index.min(), end=dados_faltosos.index.max(), freq='D') # Criar um índice de datas que cobre todo o intervalo de tempo do dataset
+dados_reindexed = dados_faltosos.reindex(date_range) # Reindexar o DataFrame para incluir todas as datas do intervalo
+dias_faltando = dados_reindexed[dados_reindexed.isnull().all(axis=1)].index # Identificar os dias que estão faltando
+print('Dias faltando no intervalo de tempo:')
+print(dias_faltando)
+
+# Existem dias faltando no intervalo de tempo, o que pode indicar que o dataset não está completo
+# Uma alternativa consiste em preencher os valores faltantes com a média dos valores anteriores e posteriores
+
+# Verificar consistência das medidas
+# Para cada hora devem haver 180 medidas, uma vez que cada medida é feita a cada 20 segundos
+contagem_medidas = dados.groupby(dados.index).count()
+print(contagem_medidas[contagem_medidas['% Iron Feed'] != 180])
+
+# Existem dias que não possuem 180 medidas, no entanto, são apenas dois dias o que não causa impacto signifiticativo
+
+# Caso se opte em não preencher os valores faltantes, uma alterativa é selecionar os dados a partir de uma data específica
+# Por exemplo, a partir de 2017-03-29, para retirar os dias faltantes e reduzir o impacto nas análises
+
+dados_tratados= dados[dados.index > '2017-03-29']
+print(dados_tratados.describe())
+print(dados_tratados.info())
+
+# EDA - Análise Exploratória de Dados
+# Análise de correlação
+matriz_correlacao  dados_tratados.corr()
+
+mask = np.triu(np.ones_like(matriz_correlacao, dtype=bool))# Criar uma máscara para o triângulo superior
+plt.figure(figsize=(20, 15))
+sns.set(font_scale=1.0)
+heatmap = sns.heatmap(matriz_correlacao, annot=True, cmap='RdYlBu', square=True,
+                      fmt=".2f",  # Exibir valores com duas casas decimais
+                      center=0,
+                      xticklabels=matriz_correlacao.columns,  # Rótulos do eixo x
+                      yticklabels=matriz_correlacao.columns,  # Rótulos do eixo y
+                      annot_kws={"size": 15},  # Tamanho da fonte dentro das células
+                      linewidths=0.5, linecolor='white',  # Bordas das células
+                      mask=mask,
+                      vmin=-1, vmax=1,  # Escala de cores de -1 a 1
+                      cbar_kws={"shrink": 1})  # Ajustar o tamanho da barra de cores
+
+
+ax = heatmap.axes# Obter os eixos atuais
+ax.set_aspect("auto")# Ajustar o aspecto dos eixos para 'auto' para ajustar automaticamente o tamanho das células
+
+plt.title("Mapa de correlação entre as variáveis")# Adicionar um título ao heatmap
+plt.savefig('Images/mapa_de_correlacao.png')
+
+# Separação das features do dataset
+
+airFlow = dados_tratados[['Flotation Column 01 Air Flow', 
+                          'Flotation Column 02 Air Flow', 
+                          'Flotation Column 03 Air Flow', 
+                          'Flotation Column 04 Air Flow',
+                          'Flotation Column 05 Air Flow', 
+                          'Flotation Column 06 Air Flow', 
+                          'Flotation Column 07 Air Flow',
+                          '% Silica Concentrate'
+                        ]].drop_duplicates().dropna().groupby('date').mean()  
+
+airLevel = dados_tratados[['Flotation Column 01 Level',
+                          'Flotation Column 02 Level', 
+                          'Flotation Column 03 Level', 
+                          'Flotation Column 04 Level', 
+                          'Flotation Column 05 Level', 
+                          'Flotation Column 06 Level', 
+                          'Flotation Column 07 Level',
+                          '% Silica Concentrate'
+                          ]].drop_duplicates().dropna().groupby('date').mean()
+
+importante = dados_tratados[['Starch Flow',
+                            'Amina Flow',
+                            'Ore Pulp Flow',
+                            'Ore Pulp pH',
+                            'Ore Pulp Density',
+                            '% Silica Concentrate',
+                            ]].drop_duplicates().dropna().groupby('date').mean()
+
+# Calcular a correlação
+correlacao = importante.corr()
+mask = np.triu(np.ones_like(correlacao, dtype=bool))  # Criar uma máscara para o triângulo superior
+
+# Criar o mapa de calor
+plt.figure(figsize=(20, 15))
+sns.set(font_scale=1.0)
+heatmap = sns.heatmap(correlacao, annot=True, cmap='RdYlBu', square=True,
+                      fmt=".2f", center=0, xticklabels=correlacao.columns,
+                      yticklabels=correlacao.columns, annot_kws={"size": 15},
+                      linewidths=0.5, linecolor='white', mask=mask, vmin=-1, vmax=1,
+                      cbar_kws={"shrink": 1})
+heatmap.axes.set_aspect("auto")
+plt.title("Mapa de correlação entre as variáveis")
+plt.savefig('Images/mapa_de_correlacao_importante.png')
+
+# Visualização das séries temporais
+cor_importante = ['#9dc6ae', '#bbd0ff', '#c8b6ff', '#deab90']
+fig, axs = plt.subplots(4, 1, figsize=(16, 18))  # Criar uma figura com 4 subplots
+fig.suptitle('Séries temporais das variáveis de interesse', fontsize=20)  # Adicionar um título à figura
+
+# Adicionar as séries temporais aos subplots
+variaveis = ['Starch Flow', 'Amina Flow', 'Ore Pulp pH', '% Silica Concentrate']
+for i, var in enumerate(variaveis):
+    axs[i].plot(importante.index, importante[var], label=var, color=cor_importante[i])
+    axs[i].set_ylabel(var)
+    axs[i].legend()
+    axs[i].set_facecolor('white')  # Definir fundo branco
+    decomposicao = seasonal_decompose(importante[var], model='additive', period=30, extrapolate_trend=30)
+    axs[i].plot(decomposicao.trend, color='red')
+
+# Adicionar rótulo ao eixo x no último subplot
+axs[-1].set_xlabel('Date')
+
+# Ajustar layout e mostrar o gráfico
+plt.tight_layout(rect=[0, 0, 1, 0.96])  # Ajustar layout para não sobrepor o título
+plt.savefig('Images/series_temporais_importante.png')
+
+# Calcular a correlação
+correlacao_airLevel = airLevel.corr()
+mask = np.triu(np.ones_like(correlacao_airLevel, dtype=bool))  # Criar uma máscara para o triângulo superior
+
+# Criar o mapa de calor
+plt.figure(figsize=(20, 15))
+sns.set(font_scale=1.0)
+heatmap = sns.heatmap(correlacao_airLevel, annot=True, cmap='RdYlBu', square=True,
+                      fmt=".2f",  # Exibir valores com duas casas decimais
+                      center=0,
+                      xticklabels=correlacao_airLevel.columns,  # Rótulos do eixo x
+                      yticklabels=correlacao_airLevel.columns,  # Rótulos do eixo y
+                      annot_kws={"size": 15},  # Tamanho da fonte dentro das células
+                      linewidths=0.5, linecolor='white',  # Bordas das células
+                      mask=mask,
+                      vmin=-1, vmax=1,  # Escala de cores de -1 a 1
+                      cbar_kws={"shrink": 1})  # Ajustar o tamanho da barra de cores
+
+# Ajustar o aspecto dos eixos para 'auto' para ajustar automaticamente o tamanho das células
+heatmap.axes.set_aspect("auto")
+
+# Adicionar um título ao heatmap
+plt.title("Mapa de correlação entre as variáveis de Air Level")
+plt.savefig('Images/mapa_de_correlacao_airLevel.png')
+
+# Visualização das séries temporais
+# DATAFRAME: airLevel
+cor_airLevel = ['#e4e7e4', '#c0c4ca', '#9ba1b0', '#777f96', '#535c7b', '#2e3961', '#0a1647','#deab90']
+fig, axs = plt.subplots(8, 1, figsize=(16, 24))  # Criar uma figura com 7 subplots
+fig.suptitle('Séries temporais das variáveis de Air Level', fontsize=20)  # Adicionar um título à figura
+
+# Adicionar as séries temporais aos subplots
+for i, coluna in enumerate(airLevel.columns):
+    axs[i].plot(airLevel.index, airLevel[coluna], label=coluna, color=cor_airLevel[i])
+    axs[i].set_ylabel(coluna)
+    axs[i].legend()
+    axs[i].set_facecolor('white')  # Definir fundo branco
+    decomposicao = seasonal_decompose(airLevel[coluna], model='additive', period=30, extrapolate_trend=30)
+    axs[i].plot(decomposicao.trend, color='red')
+
+# Adicionar rótulo ao eixo x no último subplot
+axs[-1].set_xlabel('Date')
+
+# Ajustar layout e mostrar o gráfico
+plt.tight_layout(rect=[0, 0, 1, 0.96])  # Ajustar layout para não sobrepor o título
+plt.savefig('Images/series_temporais_airLevel.png')
+
+
+# Calcular a correlação
+correlacao_airFlow = airFlow.corr()
+mask = np.triu(np.ones_like(correlacao_airFlow, dtype=bool))  # Criar uma máscara para o triângulo superior
+
+# Criar o mapa de calor
+plt.figure(figsize=(20, 15))
+sns.set(font_scale=1.0)
+heatmap = sns.heatmap(correlacao_airFlow, annot=True, cmap='RdYlBu', square=True,
+                      fmt=".2f",  # Exibir valores com duas casas decimais
+                      center=0,
+                      xticklabels=correlacao_airFlow.columns,  # Rótulos do eixo x
+                      yticklabels=correlacao_airFlow.columns,  # Rótulos do eixo y
+                      annot_kws={"size": 15},  # Tamanho da fonte dentro das células
+                      linewidths=0.5, linecolor='white',  # Bordas das células
+                      mask=mask,
+                      vmin=-1, vmax=1,  # Escala de cores de -1 a 1
+                      cbar_kws={"shrink": 1})  # Ajustar o tamanho da barra de cores
+
+# Ajustar o aspecto dos eixos para 'auto' para ajustar automaticamente o tamanho das células
+heatmap.axes.set_aspect("auto")
+
+# Adicionar um título ao heatmap
+plt.title("Mapa de correlação entre as variáveis de Air Flow")
+plt.savefig('Images/mapa_de_correlacao_airFlow.png')
+
+# Visualização das séries temporais
+# DATAFRAME: airFlow
+cor_airFlow = ['#edf2fb', '#e2eafc', '#d7e3fc', '#ccdbfd', '#c1d3fe', '#b6ccfe', '#abc4ff','#deab90']
+fig, axs = plt.subplots(8, 1, figsize=(16, 24))  # Criar uma figura com 7 subplots
+fig.suptitle('Séries temporais das variáveis de Air Flow', fontsize=20)  # Adicionar um título à figura
+
+# Adicionar as séries temporais aos subplots
+for i, coluna in enumerate(airFlow.columns):
+    axs[i].plot(airFlow.index, airFlow[coluna], label=coluna, color=cor_airFlow[i])
+    axs[i].set_ylabel(coluna)
+    axs[i].legend()
+    axs[i].set_facecolor('white')  # Definir fundo branco
+    decomposicao = seasonal_decompose(airFlow[coluna], model='additive', period=30, extrapolate_trend=30)
+    axs[i].plot(decomposicao.trend, color='red')
+
+# Adicionar rótulo ao eixo x no último subplot
+axs[-1].set_xlabel('Date')
+
+# Ajustar layout e mostrar o gráfico
+plt.tight_layout(rect=[0, 0, 1, 0.96])  # Ajustar layout para não sobrepor o título
+plt.savefig('Images/series_temporais_airFlow.png')
